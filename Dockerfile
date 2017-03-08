e53bf81373db7738e71e06e88d3c292b60cb5d64
Tobias Brunner <tobias@tobru.ch>
2017-03-08 21:08:03 +0100

    replace original entrypoint script

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM nextcloud:11-fpm
 RUN chgrp -R 0 /var/www/html && \
     chmod -R g+rwX /var/www/html
 
+COPY docker-entrypoint.sh /entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,6 @@ set -e
 if [ ! -e '/var/www/html/version.php' ]; then
   echo "Extracting app source to /var/www/html"
   tar cf - --one-file-system -C /usr/src/nextcloud . | tar xf - --no-overwrite-dir
-  #chown -R www-data /var/www/html
 fi
 
 exec "$@"
